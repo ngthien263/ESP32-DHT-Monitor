15cd5e2c12f7bd69123c5b70098534b302388e92
ngthien263 <vuongngocthien222@gmail.com>
2025-08-22 21:37:09 +0700

    doc for common_observer and common subject

--- components/common/include/common_observer.h
+++ components/common/include/common_observer.h
@@ -1,5 +1,19 @@
-#ifndef OBSERVER_H
-#define OBSERVER_H
+/**
+ * @file common_observer.h
+ * @brief Generic Observer helper types and inline APIs for the Observer pattern.
+ *
+ * This module defines a lightweight observer wrapper that holds:
+ *  - A pointer to a Subject (publisher),
+ *  - A RTOS task handle (for notify-to-task patterns),
+ *  - A notification callback invoked by the Subject.
+ *
+ * It provides inline helpers to set the subject and (un)subscribe.
+ * The concrete observer object should set @ref observer_t.self and @ref observer_t.on_notify
+ * before subscribing.
+ */
+
+#ifndef COMMON_OBSERVER_H
+#define COMMON_OBSERVER_H
 
 #include "common_subject.h"
 #include "freertos/task.h"
@@ -7,25 +21,62 @@
 extern "C" {
 #endif
 
+/**
+ * @struct observer_ctx_t
+ * @brief Runtime context for an observer.
+ *
+ */
 typedef struct {
     subject_t* subject;            
     TaskHandle_t task_handle;      
 } observer_ctx_t;
 
+
+/**
+ * @struct observer_t
+ * @brief Generic Observer “base class”.
+ * 
+ */
 typedef struct {
     void* self;
     observer_ctx_t ctx;
     void (*on_notify)(void*);
 } observer_t;
 
+
+/**
+ * @brief Wrapper to bind an observer to a subject (no subscription yet).
+ *
+ * @param[in,out] observer Observer to bind.
+ * @param[in]     subject  Subject to observe.
+ *
+ * @warning This does not perform subscription. Call @ref observer_subscribe next.
+ */
 static inline void observer_set_subject(observer_t* observer, subject_t* subject){
     observer->ctx.subject = subject;
 }
 
+/**
+ * @brief Subscribe the observer to its bound subject.
+ *
+ * Invokes Subject’s subscribe with @ref observer_t.self and @ref observer_t.on_notify.
+ * @note Ensure both fields are set before calling.
+ *
+ * @param[in,out] observer Observer to subscribe.
+ *
+ */
 static inline void observer_subscribe(observer_t* observer) {
     subject_subscribe(observer->ctx.subject, observer->self, observer->on_notify);
 } 
 
+/**
+ * @brief Unsubscribe the observer from its bound subject.
+ *
+ * Removes the callback from the Subject’s subscriber list.
+ *
+ * @param[in,out] observer Observer to unsubscribe.
+ *
+ */
 static inline void observer_unsubscribe(observer_t* observer) {
     subject_unsubscribe(observer->ctx.subject, observer->on_notify);
 } 
@@ -33,4 +84,4 @@ static inline void observer_unsubscribe(observer_t* observer) {
 #ifdef __cplusplus
 }
 #endif
-#endif // OBSERVER_H
+#endif // COMMON_OBSERVER_H
